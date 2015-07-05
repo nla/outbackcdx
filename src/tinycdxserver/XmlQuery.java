ecd5bd90fdf45b214829540a7ee525db2f46038f
Alex Osborne <asoborne@nla.gov.au>
2015-07-06 08:55:24 +1000

    Match wayback's encoding properly when deocding query

--- src/tinycdxserver/XmlQuery.java
+++ src/tinycdxserver/XmlQuery.java
@@ -4,6 +4,8 @@ import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 import java.io.BufferedOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.HashMap;
@@ -37,15 +39,20 @@ public class XmlQuery {
     }
 
     private static Map<String,String> decodeQueryString(String q) {
-
-        Map<String,String> m = new HashMap<>();
-        for (String entry : q.split(" ")) {
-            String[] fields = entry.split(":", 2);
-            String key = UrlCanonicalizer.urlDecode(fields[0]);
-            String value = UrlCanonicalizer.urlDecode(fields[1]);
-            m.put(key, value);
+        try {
+            Map<String,String> m = new HashMap<>();
+            for (String entry : q.split(" ")) {
+                String[] fields = entry.split(":", 2);
+                // we use URLDecoder rather than URLCanonicalize here to match Wayback's encoding behaviour
+                // (spaces encoded as + rather than %20)
+                String key = URLDecoder.decode(fields[0], "UTF-8");
+                String value = URLDecoder.decode(fields[1], "UTF-8");
+                m.put(key, value);
+            }
+            return m;
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
         }
-        return m;
     }
 
     private static void writeElement(XMLStreamWriter out, String name, Object value) throws XMLStreamException {
