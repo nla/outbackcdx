e8b3e07c2541c1812262a0b0b29eb0cf66b3d3f3
Alex Osborne <ato@meshy.org>
2016-10-17 10:19:49 +1100

    Remove debug sleep

--- test-integration/test-pywb.sh
+++ test-integration/test-pywb.sh
@@ -38,5 +38,3 @@ PAGE=http://data.webarchive.org.uk/crawl-test-site/
 check $WAYBACK_URL/*/$PAGE 20161016214133
 check $WAYBACK_URL/*/$PAGE 20161016214156
 #check $WAYBACK_URL/20161016214133/$PAGE 'Simple Text Documents'
-
-sleep 6000
