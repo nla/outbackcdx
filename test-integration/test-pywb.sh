c35b9bdc5d5c7e74baecf314d03bb20a1ba7639a
Alex Osborne <aosborne@nla.gov.au>
2018-05-02 16:01:39 +0900

    Update integration tests for newer pywb

--- test-integration/test-pywb.sh
+++ test-integration/test-pywb.sh
@@ -38,6 +38,6 @@ wait_until_listening http://localhost:8080/testcol
 WAYBACK_URL=http://localhost:8080/testcol
 PAGE=http://data.webarchive.org.uk/crawl-test-site/
 
-check $WAYBACK_URL/*/$PAGE 20161016214133
-check $WAYBACK_URL/*/$PAGE 20161016214156
+check "$WAYBACK_URL/cdx?url=$PAGE" 20161016214133
+check "$WAYBACK_URL/cdx?url=$PAGE" 20161016214156
 #check $WAYBACK_URL/20161016214133/$PAGE 'Simple Text Documents'
