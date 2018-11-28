f660d5464be0690e35ce8c8de02dafbc85d558fe
Alex Osborne <aosborne@nla.gov.au>
2018-11-28 21:58:16 +1100

    Reject too large requests rather than infinite looping #43

--- src/outbackcdx/NanoHTTPD.java
+++ src/outbackcdx/NanoHTTPD.java
@@ -627,7 +627,7 @@ public abstract class NanoHTTPD {
         public enum Status implements IStatus {
             SWITCH_PROTOCOL(101, "Switching Protocols"), OK(200, "OK"), CREATED(201, "Created"), ACCEPTED(202, "Accepted"), NO_CONTENT(204, "No Content"), PARTIAL_CONTENT(206, "Partial Content"), REDIRECT(301,
                     "Moved Permanently"), NOT_MODIFIED(304, "Not Modified"), BAD_REQUEST(400, "Bad Request"), UNAUTHORIZED(401,
-                    "Unauthorized"), FORBIDDEN(403, "Forbidden"), NOT_FOUND(404, "Not Found"), METHOD_NOT_ALLOWED(405, "Method Not Allowed"), RANGE_NOT_SATISFIABLE(416,
+                    "Unauthorized"), FORBIDDEN(403, "Forbidden"), NOT_FOUND(404, "Not Found"), METHOD_NOT_ALLOWED(405, "Method Not Allowed"), PAYLOAD_TOO_LARGE(413, "Payload Too Large"), RANGE_NOT_SATISFIABLE(416,
                     "Requested Range Not Satisfiable"), INTERNAL_ERROR(500, "Internal Server Error");
             private final int requestStatus;
             private final String description;
@@ -772,6 +772,9 @@ public abstract class NanoHTTPD {
                     }
                     while (read > 0) {
                         rlen += read;
+                        if (rlen >= BUFSIZE) {
+                            throw new ResponseException(Response.Status.PAYLOAD_TOO_LARGE, "Request too large");
+                        }
                         splitbyte = findHeaderEnd(buf, rlen);
                         if (splitbyte > 0)
                             break;
