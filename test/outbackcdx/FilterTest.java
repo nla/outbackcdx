9076652e2649e24f0aeb1ec1fe7a494ed7c01d45
Noah Levitt <nlevitt@archive.org>
2019-09-23 12:42:08 -0700

    testCollapse()

--- test/outbackcdx/FilterTest.java
+++ test/outbackcdx/FilterTest.java
@@ -41,6 +41,28 @@ public class FilterTest {
         assertTrue(Filter.fromSpec("~!status:201").test(two));
     }
 
+    @Test
+    public void testCollapse() {
+        Capture one = new Capture();
+        one.original = "http://example.com/collapse";
+        one.timestamp = 20190101000000l;
+        Capture two = new Capture();
+        two.original = "http://example.com/collapse";
+        two.timestamp = 20190101000005l;
+
+        Filter originalCollapser = Filter.collapser("original");
+        assertTrue(originalCollapser.test(one));
+        assertFalse(originalCollapser.test(two));
+
+        Filter t14Collapser = Filter.collapser("timestamp");
+        assertTrue(t14Collapser.test(one));
+        assertTrue(t14Collapser.test(two));
+
+        Filter t13Collapser = Filter.collapser("timestamp:13");
+        assertTrue(t13Collapser.test(one));
+        assertFalse(t13Collapser.test(two));
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void bogusField() {
         Filter.fromSpec("bogus:.*");
